efe216c2a778003e4dfd4e9e3ed5837b7d5a8506
aryanbhatnagar0102 <aryanbhatnagar@gmail.com>
2026-09-01 10:20:39 +0530

    update

--- lab3/prg9.js
+++ lab3/prg9.js
@@ -1,7 +1,31 @@
-import http from 'http';
-
-const server = http.createServer((req,res)=>{
-    res.end("<h1>Hello client<?h1>");
-});
-
-server.listen(3000,() => console.log("prg9"));
+import http from 'http'
+import { createReadStream } from 'fs';
+const server=http.createServer((req,res)=>
+{
+    if(req.url=="/")
+    {
+        res.setHeader("content-type","text/html")
+        res.statusCode=200
+    const stream=createReadStream('./pages/airtag.html',{encoding:"utf-8"}) 
+    stream.pipe(res)
+    }
+    else if(req.url==="/mobile")
+    {
+        res.writeHead(200,{"content-type":"text/json"})
+        const stream=createReadStream('./data/product.json',{encoding:"utf-8"})
+        stream.pipe(res)
+    }
+    else if(req.url==="/manual")
+    {
+        res.writeHead(200,{"content-type":"text/plain"})
+        const stream=createReadStream('./data/chatgpt.txt',{encoding:"utf-8"})
+        stream.pipe(res)
+    }
+    
+    else
+    {
+        res.statusCode=404
+        res.end("Not found")
+    }
+})
+server.listen(3000,()=>console.log("Server is running...."))
